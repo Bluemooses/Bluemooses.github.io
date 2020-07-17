e1729deb020fe5b9d22de24d29bae57e2c2203b9
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-17 14:57:41 -0500

    This APP uses SASS

--- self-page/src/App/App.js
+++ self-page/src/App/App.js
@@ -2,47 +2,17 @@ import React from "react";
 import axios from "axios";
 
 import "./App.css";
-import SocialFollow from "../Components/SocialFollow";
+import Header from "../Components/Header/Header";
+
+import Footer from "../Components/Footer/Footer";
 
 class App extends React.Component {
-  state = { advice: "" };
-
-  componentDidMount() {
-    this.fetchAdvice();
-  }
-
-  fetchAdvice = () => {
-    axios
-      .get("https://api.adviceslip.com/advice")
-      .then((response) => {
-        const { advice } = response.data.slip;
-        this.setState({ advice: advice });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   render() {
-    const { advice } = this.state;
     return (
-      <div>
-        <div className="app">
-          <h1>WELCOME TO GYAANI BABA</h1>
-          <h3>
-            Your personal advisor to keep you motivated and calm amidst chaos
-          </h3>
-          <div className="card">
-            <h2>{advice}</h2>
-            <button className="buton" onClick={this.fetchAdvice}>
-              <span>
-                <h4>Unlock today's Gyaan</h4>
-              </span>
-            </button>
-          </div>
-        </div>
+      <div className="app">
         <footer>
-          <SocialFollow />
+          <Header />
+          <Footer />
         </footer>
       </div>
     );
