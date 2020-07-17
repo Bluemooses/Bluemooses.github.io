76b0245914eedd80f48139d570039948a175e44f
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-17 15:04:21 -0500

    BACKGROUND IMAGE

--- self-page/src/App/App.js
+++ self-page/src/App/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-import "./App.css";
+import "./App.scss";
 import Header from "../Components/Header/Header";
 
 import Footer from "../Components/Footer/Footer";
